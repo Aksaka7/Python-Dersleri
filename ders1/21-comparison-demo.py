1886568121040538b5678934030caf23e875c45e
Aksaka7 <aksaka7@gmail.com>
2023-02-03 20:43:10 +0100

    Son yapılan işlemler

--- ders1/21-comparison-demo.py
+++ ders1/21-comparison-demo.py
@@ -1,17 +1,60 @@
 
 # 1- Girilen 2 sayıdan hangisi büyüktür ?
 
+# number1 = int(input("Sayı gir: "))
+# number2 = int(input("Sayı gir: "))
+
+# if number1 > number2:
+#     print("bu sayi büyüktür : " + str(number1))
+# if number1 < number2:
+#     print("Bu sayi daha büyüktür  " + str(number2))
 
 
 
 # 2- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.
 #    Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
 
+# vize = float(input("Vize Notu: "))
+# final = float(input("Final Notu: "))
+
+# result = ((vize*0.6)+(final*0.4))
+# print(result)
+# if result > 50:
+#     print("Sınıfı Geçtiniz")
+# if result < 50:
+#     print("Sınıfta Kaldınız")
 
 # 3- Girilen bir sayının tek mi çift mi olduğunu yazdırın.
 
+# sayi = float(input("Sayı gir: "))
 
-# 4- Girilen bir sayının negatif pozitif durumunu yazdırın.
+# if sayi % 2 == 0:
+#     print("Bu sayi Çifttir : " + str(sayi))
+# else:
+#     print("Bu sayi Tektir : " + str(sayi))   
+
+
+# 4- Girilen bir sayının negatif pozitif durumunu yazdırın. 
+
+# sayi = int(input("Sayı gir: "))
+
+# if sayi > 0:
+#     print("Bu sayi Pozitif Bir sayıdır")
+# if sayi < 0:
+#     print("Bu sayi Negatif Bir sayidir.")
+# if sayi == 0: 
+#     print("Sıfır nötür bir sayıdır")
 
 # 5- Parola ve email bilgisini isteyip doğruluğunu kontrol ediniz.
 #    (email: email@erensunar.com parola:abc123)
+
+# mail = "email@erensunar.com"
+# parola = "abc123"
+
+# check_Mail = input("Mail Adresinizi giriniz: ")
+# check_Parola = input("Parolanızı giriniz Lütfen: ")
+
+# if mail == check_Mail and parola == check_Parola:
+#     print("Sisteme Şuanda Giriş yaptınız")
+# else:
+#     print("Bilgileriniz Yanlış tekrar deneyiniz.")
--- ders1/23-logical-demo.py
+++ ders1/23-logical-demo.py
@@ -1,19 +1,59 @@
 
 # 1- Girilen bir sayının 0-100 arasında olup olmadığını kontrol ediniz.
+# number = int(input("Sayi gir: "))
 
+# if number < 100 and number >= 0:
+#     print(str(number) + " : Bu sayi 0-100 arasındadır. ")
+# if number < 0:
+#     print("Bu sayi 0 dan küçüktür.")
+# if number > 100:
+#     print("Bu sayi 100 den büyüktür. ")
 
 # 2- Girilen bir sayının pozitif çift sayı olup olmadığını kontrol ediniz.
+# number = int(input("Sayi gir: "))
+
+# if number >= 0 and number % 2 == 0:
+#     print("Bu sayi Pozitif ve Çiftir " + str(number))
+# else:
+#     print("Bu sayi arama kriterlerine uymuyor.")
 
 
-# 3- Email ve parola bilgileri ile giriş kontrolü yapınız. 
+# 3- Email ve parola bilgileri ile giriş kontrolü yapınız.
+
+# mail = "besir@mehmet.com"
+# parola = "abc123"
+
+# check_Mail = input("Mail Adresinizi giriniz: ")
+# check_Parola = input("Parolanızı giriniz Lütfen: ")
+
+# if mail == check_Mail and parola == check_Parola:
+#     print("Sisteme Şuanda Giriş yaptınız")
+# else:
+#     print("Bilgileriniz Yanlış tekrar deneyiniz.")
 
 
+# # 4- Girilen 3 sayıyı büyüklük olarak karşılaştırınız.
+# # birinci_sayi = int(input("Bir sayı giriniz: "))
+# # ikinci_sayi = int(input("Bir sayı giriniz: "))
+# # ucuncu_sayi = int(input("Bir sayı giriniz: "))
 
-# 4- Girilen 3 sayıyı büyüklük olarak karşılaştırınız.
+# # if birinci_sayi > ikinci_sayi and birinci_sayi > ucuncu_sayi:
+# #     print(f"Girilen birinci sayi Buyuktur: {birinci_sayi}")
+
+# # if ikinci_sayi > birinci_sayi and ikinci_sayi > ucuncu_sayi:
+# #     print(f"Girilen İkinci sayi buyuktur: {ikinci_sayi}")
+
+# # else:
+# #     print(f"girilen üçüncü sayi büyüktür: {ucuncu_sayi}")
 
 # 5- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.
 
+# # vize = float(input("Vize notunu giriniz: "))
+# # final = float(input("Final notunu giriniz: "))
+
+# # result = (vize*0.6)+(final*0.4) 
+
+# # print(result)
 
 
 # 6- Kişinin ad, kilo ve boy bilgilerini alıp kilo indekslerini hesaplayınız.
-
